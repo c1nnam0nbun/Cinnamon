be36eec93d21f4178cdcb55109e2de1faffd42d2
c1nnam0nbun <c1nnam0nbun270@gmail.com>
2024-08-14 20:50:04 +0300

    Minor changes
    Modified Application to actually invoke init function and started laying out systems API

--- Core/src/main/kotlin/com/c1nnam0nbun/cinnamon/Application.kt
+++ Core/src/main/kotlin/com/c1nnam0nbun/cinnamon/Application.kt
@@ -1,9 +1,12 @@
 package com.c1nnam0nbun.cinnamon
 
 class Application private constructor() {
+    val world = World()
+
     companion object {
         operator fun invoke(init: Application.() -> Unit) {
-
+            val app = Application()
+            app.apply(init)
         }
     }
 }
--- Sandbox/src/main/kotlin/com/c1nnam0nbun/cinnamon/Main.kt
+++ Sandbox/src/main/kotlin/com/c1nnam0nbun/cinnamon/Main.kt
@@ -1,5 +1,36 @@
 package com.c1nnam0nbun.cinnamon
 
-fun main() = Application {
+annotation class SystemConfig(
+    val stage: String = "",
+    val runOnce: Boolean = false,
+    val before: Array<String> = [],
+    val after: Array<String> = [],
+)
 
+class Commands
+
+@SystemConfig
+fun system(commands: Commands) {
+
+}
+
+@SystemConfig
+fun immediateSystem(world: World) {
+
+}
+
+class ComponentA : Component<ComponentA> {
+    override val type: ComponentID<ComponentA> = ComponentA
+    companion object : ComponentID<ComponentA>()
+}
+
+class ComponentB : Component<ComponentB> {
+    override val type: ComponentID<ComponentB> = ComponentB
+    companion object : ComponentID<ComponentB>()
+}
+
+fun main() = Application {
+    val e = world.createEntity(ComponentA())
+    world.addComponent(e, ComponentB())
+    world.removeComponent(e, ComponentA)
 }
